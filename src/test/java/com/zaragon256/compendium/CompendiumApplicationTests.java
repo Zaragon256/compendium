1972b404d4495eb175c547e482402345168bbe93
Zaragon256 <luis.arenas.zu@gmail.com>
2023-02-13 16:01:52 -0500

    Initial commit

---
+++ src/test/java/com/zaragon256/compendium/CompendiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaragon256.compendium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompendiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
